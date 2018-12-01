19cfe2f019fdfa52d8568264bd2dc9a2532ceab9
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-12-01 12:23:12 -0500

    book model

---
+++ models/book.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  var Book = sequelize.define("Book", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 300]
+      }
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1, 300]
+    },
+    genres: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1, 300]
+    },
+    isbn: {
+      type: DataTypes.INT,
+      allowNull: true
+    }
+  });
+};
