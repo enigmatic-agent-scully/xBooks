98e80393ee920c4bd2715ed12a5479a3211d606b
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-12-05 21:22:48 -0500

    incorporated front-end layout

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,8 +2,6 @@ var path = require("path");
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
-    res.render("index", {
-      books: req
-    });
+    res.sendFile(path.join(__dirname, "../public/index.handlebars"));
   });
 };
