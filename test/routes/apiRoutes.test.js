b87db4463c0458b2f09623d8ad52c5ace03e6a9c
AJ <amanda.elisabet.jordan@gmail.com>
2018-12-13 21:17:09 -0500

    just saving

--- test/routes/apiRoutes.test.js
+++ test/routes/apiRoutes.test.js
@@ -3,7 +3,6 @@ var chaiHttp = require('chai-http');
 var server = require('../server.test');
 var db = require('./../models');
 var expect = chai.expect;
-var mocha = require('mocha');
 
 chai.use(chaiHttp);
 
@@ -69,46 +68,44 @@ describe('GET /api/books', () => {
   });
 });
 
-// var db = require('../models');
+module.exports = function(app) {
+  app.get('/api/books', function(req, res) {
+    db.Book.findAll({}).then(dbBooks => {
+      res.json(dbBooks);
+    });
+  });
 
-// module.exports = function(app) {
-//   app.get('/api/books', function(req, res) {
-//     db.Book.findAll({}).then(dbBooks => {
-//       res.json(dbBooks);
-//     });
-//   });
+  app.post('/api/books', function(req, res) {
+    console.log(req.body);
+    db.Book.create({
+      title: req.body.title,
+      author: req.body.author,
+      genres: req.body.genres,
+      isbn: req.body.isbn,
+      coverimg: req.body.coverimg,
+      pubDate: req.body.pubDate
+    }).then(function(dbBook) {
+      res.json(dbBook);
+    });
+  });
 
-//   app.post('/api/books', function(req, res) {
-//     console.log(req.body);
-//     db.Book.create({
-//       title: req.body.title,
-//       author: req.body.author,
-//       genres: req.body.genres,
-//       isbn: req.body.isbn,
-//       coverimg: req.body.coverimg,
-//       pubDate: req.body.pubDate
-//     }).then(function(dbBook) {
-//       res.json(dbBook);
-//     });
-//   });
+  app.put('/api/books', function(req, res) {
+    db.Book.update(req.body, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbBook) {
+      res.json(dbBook);
+    });
+  });
 
-//   app.put('/api/books', function(req, res) {
-//     db.Book.update(req.body, {
-//       where: {
-//         id: req.body.id
-//       }
-//     }).then(function(dbBook) {
-//       res.json(dbBook);
-//     });
-//   });
-
-//   app.delete('/api/books/:id', (req, res) => {
-//     db.Book.destroy({
-//       where: {
-//         id: req.params.id
-//       }
-//     }).then(dbBook => {
-//       res.json(dbTodo);
-//     });
-//   });
-// };
+  app.delete('/api/books/:id', (req, res) => {
+    db.Book.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(dbBook => {
+      res.json(dbTodo);
+    });
+  });
+};
